17897b9c57f935842d819da45fb66c8a80fdd3d1
HarunPasuli <harunpasuli24@gmail.com>
2024-01-26 16:28:19 +0100

    s

--- 4-natours/starter/public/js/bundle.js
+++ 4-natours/starter/public/js/bundle.js
@@ -12284,6 +12284,7 @@ var loginForm = document.querySelector(".form--login");
 var logOutBtn = document.querySelector(".nav__el--logout");
 var userDataForm = document.querySelector(".form-user-data");
 var userPasswordForm = document.querySelector(".form-user-password");
+var bookBtn = document.getElementById("book-tour");
 
 // DELEGATION
 if (mapBox) {
@@ -12299,12 +12300,12 @@ if (loginForm) loginForm.addEventListener("submit", function (e) {
 if (logOutBtn) logOutBtn.addEventListener("click", _login.logout);
 if (userDataForm) userDataForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  (0, _updateSettings.updateSettings)({
-    name: name,
-    email: email
-  }, "data");
+  var form = new FormData();
+  form.append("name", document.getElementById("name").value);
+  form.append("email", document.getElementById("email").value);
+  form.append("photo", document.getElementById("photo").files[0]);
+  console.log(form);
+  (0, _updateSettings.updateSettings)(form, "data");
 });
 if (userPasswordForm) userPasswordForm.addEventListener("submit", /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee(e) {
@@ -12363,7 +12364,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3738" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "2779" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
